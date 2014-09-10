3bb18ace6319ad43f11e842687f26012cb63675c
rudyjason <rudyjason.ep@hotmail.com>
2014-09-10 13:43:15 +0200

    controller changes

--- MediCare/Medicare.Controller/BikeController.cs
+++ MediCare/Medicare.Controller/BikeController.cs
@@ -8,12 +8,24 @@ namespace Medicare.Controller
 {
     public class BikeController
     {
+        private ComController cc;
+        private int _currentPower;
         public BikeController(string comPort)
         {
             comPort = "COM5"; //TO DO: remove
-            ComController cc = new ComController(comPort);
+            cc = new ComController(comPort);
+            cc.openConnection();
         }
 
+        public string[] GetStatus()
+        {
+            cc.send("st"); 
+            string raw = cc.read();
+            string[] rawArray = raw.Split();
+            rawArray[3] = (float.Parse(rawArray[3]) / 10).ToString();
+            rawArray[4] = (float.Parse(rawArray[4])).ToString();
 
+            return rawArray;
+        }
     }
 }
