9fafa166657b08e2c8839b31bb32b27ca412b95a
badenCA <ca.baden@student.avans.nl>
2014-09-10 14:00:37 +0200

    enums
    enum fixed

--- MediCare/Medicare.Controller/Enums.cs
+++ MediCare/Medicare.Controller/Enums.cs
@@ -8,20 +8,47 @@ namespace Medicare.Controller
 {
     class Enums
     {
-        [Flags]
+
         public enum BikeCommands
         { 
-            RESET = "rs",
-            LOCK = "lb",
-            STATUS = "st",
-            POWER = "pw",
-            CONTROLMODE = "cm",
-            ID = "id",
-            TYPE = "ki",
-            SETTIME = "pt",
-            SETDISTANCE = "pd",
-            SETENERGY = "pe",
-            GETDATETIME = "tr"
+            [StringValue("rs")]
+            RESET,
+            [StringValue("lb")]
+            LOCK,
+            [StringValue("st")]
+            STATUS,
+            [StringValue("pw")]
+            POWER,
+            [StringValue("cm")]
+            CONTROLMODE,
+            [StringValue("id")]
+            ID,
+            [StringValue("ki")]
+            TYPE,
+            [StringValue("pt")]
+            SETTIME,
+            [StringValue("pd")]
+            SETDISTANCE,
+            [StringValue("pe")]
+            SETENERGY,
+            [StringValue("tr")]
+            GETDATETIME
         };
+
+        public class StringValue : System.Attribute
+        {
+            private string _value;
+
+            public StringValue(string value)
+            {
+                _value = value;
+            }
+
+            public string Value
+            {
+                get { return _value; }
+            }
+
+        }
     }
 }
