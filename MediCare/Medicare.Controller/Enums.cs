cee9b1304deaf275415b774b100ca116e5213e0e
badenCA <ca.baden@student.avans.nl>
2014-09-10 13:16:31 +0200

    enums
    added enums

---
+++ MediCare/Medicare.Controller/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicare.Controller
+{
+    class Enums
+    {
+        [Flags]
+        public enum BikeCommands
+        { 
+            RESET = "rs",
+            LOCK = "lb",
+            STATUS = "st",
+            POWER = "pw",
+            CONTROLMODE = "cm",
+            ID = "id",
+            TYPE = "ki",
+            SETTIME = "pt",
+            SETDISTANCE = "pd",
+            SETENERGY = "pe",
+            GETDATETIME = "tr"
+        };
+    }
+}
