d20fa6c832467e4fc64402044a857e7cbf78675a
badenCA <ca.baden@student.avans.nl>
2014-09-10 14:13:12 +0200

    enums
    fixed enums

--- MediCare/Medicare.Controller/Enums.cs
+++ MediCare/Medicare.Controller/Enums.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,5 +51,29 @@ namespace Medicare.Controller
             }
 
         }
+        
+        public static string GetValue(Enum value)
+        {
+            string output = null;
+            Type type = value.GetType();
+            
+            //Check first in our cached results...
+
+            //Look for our 'StringValueAttribute' 
+
+            //in the field's custom attributes
+
+            FieldInfo fi = type.GetField(value.ToString());
+            StringValue[] attrs =
+                fi.GetCustomAttributes(typeof(StringValue),
+                                           false) as StringValue[];
+                if (attrs.Length > 0)
+                {
+                    output = attrs[0].Value;
+                }
+
+                return output;
+            }
+        }
     }
-}
+
