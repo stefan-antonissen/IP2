4c6be067853d8314d322d15b9dd9235387655d69
rudyjason <rudyjason.ep@hotmail.com>
2014-09-10 11:12:13 +0200

    comcontroller added, name change made

---
+++ MediCare/Medicare.Controller/ComController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.Ports;
+
+public class ComController
+{
+    private SerialPort _comPort;
+
+	public ComController(string port)
+	{
+        _comPort = new SerialPort(port);
+	}
+
+    public void openConnection()
+    {
+        _comPort.Open();
+    }
+
+    public void closeConnection() 
+    {
+        _comPort.Close();
+    }
+
+    public void send(string command)
+    {
+        _comPort.Write(command);
+    }
+
+    public string[] getAvailablePorts()
+    {
+        return SerialPort.GetPortNames();
+    }
+
+    public string read() 
+    {
+        return _comPort.ReadExisting();
+    }
+}
