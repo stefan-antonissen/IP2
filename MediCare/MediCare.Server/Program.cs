734f7cb4006a2325f240272ff110465b3ad83582
Jack Evers <jackxl@hotmail.com>
2014-09-12 20:34:21 +0200

    Changed Controller namespace to MediCare, added BikeSim Project

--- MediCare/MediCare.Server/Program.cs
+++ MediCare/MediCare.Server/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Medicare.Controller;
+using MediCare.Controller;
 
 namespace MediCare.Server
 {
--- MediCare/Medicare.Controller/Enums.cs
+++ MediCare/Medicare.Controller/Enums.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Medicare.Controller
+namespace MediCare.Controller
 {
     public class Enums
     {
