7e1ddb77f7e894437f1dcdea7cf25a00c5328af7
Jack Evers <jackxl@hotmail.com>
2014-09-13 13:03:56 +0200

    Changes for bikeSim

--- MediCare/Medicare.Controller/BikeController.cs
+++ MediCare/Medicare.Controller/BikeController.cs
@@ -11,9 +11,18 @@ namespace MediCare.Controller
         private ComController cc;
         public BikeController(string comPort)
         {
-            cc = new ComController(comPort);
-            cc.openConnection();
-            //Console.WriteLine(Enums.GetValue(Enums.BikeCommands.CONTROLMODE));
+            if (comPort.Contains("COM"))
+            {
+                cc = new ComController(comPort);
+                cc.openConnection();
+                //Console.WriteLine(Enums.GetValue(Enums.BikeCommands.CONTROLMODE));
+            }
+            else
+            {
+                //TODO change content of ComController to SerialController EXTENDS ComController. and add BikeSimulator EXTENDS ComController class.
+                // cc gets type ComController and gets object of either SerialController or BikeSimulator.. Bikesimulator will be made in a way that it responds accordingly to cc.send() methods etc..
+            }
+
         }
 
         #region getters
