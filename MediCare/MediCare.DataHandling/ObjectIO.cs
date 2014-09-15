72f4eb49f1ee4682eae995dae818ba3174eabe46
Frank <fja.vanveen@student.avans.nl>
2014-09-15 15:07:16 +0200

    Updated FileIO with Dennis Needs to be tested though.

--- MediCare/MediCare.DataHandling/ObjectIO.cs
+++ MediCare/MediCare.DataHandling/ObjectIO.cs
@@ -1,13 +1,70 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MediCare.DataHandling
 {
-    class ObjectIO
+    [Serializable()]
+    public class ObjectIO
     {
+        public ArrayList measurements;
+        Serializer serializer;
 
+        public ObjectIO()
+        {
+            measurements = new ArrayList();
+            serializer = new Serializer();
+        }
+
+        public void add(Measurement m)
+        {
+            measurements.Add(m);
+        }
+
+        public void delete(int index)
+        {
+            measurements.RemoveAt(index);
+        }
+
+        //Will be inserted AFTER the given index
+        public void insert(int index, Measurement m)
+        {
+            measurements.Insert(index, m);
+        }
+
+        public void empty()
+        {
+            measurements.Clear();
+        }
+
+        // to save diskspace always trim before writing to disk
+        public void trim(int size)
+        {
+            measurements.TrimToSize();
+        }
+
+        public int getSize()
+        {
+            return measurements.Capacity;
+        }
+
+        public void writeToDisk()
+        {
+            serializer.SerializeObject("UrFile.Dat", measurements);
+        }
+
+        public ArrayList ReadFromDisk() {
+            ArrayList result = serializer.DeSerializeObject("UrFile.Dat");
+
+            return result;
+        }
     }
+
+
 }
---
+++ MediCare/MediCare.DataHandling/Serializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCare.DataHandling
+{
+    public class Serializer
+    {
+        public Serializer()
+        {
+        }
+
+        public void SerializeObject(string filename, ArrayList objectToSerialize)
+        {
+            Stream stream = File.Open(filename, FileMode.Create);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(stream, objectToSerialize);
+            stream.Close();
+        }
+
+        public ArrayList DeSerializeObject(string filename)
+        {
+            ArrayList objectToSerialize;
+            Stream stream = File.Open(filename, FileMode.Open);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            objectToSerialize = (ArrayList)bFormatter.Deserialize(stream);
+            stream.Close();
+            return objectToSerialize;
+        }
+    }
+}
