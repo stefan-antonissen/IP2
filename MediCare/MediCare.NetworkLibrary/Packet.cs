fed776311ba08d3ce8c166438c412c2454985b7f
Stefan Antonissen <stefan.antonissen@gmail.com>
2014-09-24 11:46:20 +0200

    Packets

---
+++ MediCare/MediCare.NetworkLibrary/Packet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCare.NetworkLibrary
+{
+    [Serializable()]
+    public class Packet
+    {
+        private string data;
+
+        public Packet(string data)
+        {
+            this.data = data;
+        }
+
+        public void handleServer(TcpClient tcpClient, ServerInterface server)
+        {
+            server.send(tcpClient);
+        }
+
+        public void handleClient(ClientInterface client)
+        {
+            client.send(data);
+        }
+    }
+}
