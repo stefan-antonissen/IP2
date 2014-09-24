6ae6edadf570dbbd197a027b79587c18013e7c6d
rudyjason <rudyjason.ep@hotmail.com>
2014-09-24 11:28:33 +0200

    initial server settings

--- MediCare/MediCare.Server/Program.cs
+++ MediCare/MediCare.Server/Program.cs
@@ -4,38 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediCare.Controller;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
 
 namespace MediCare.Server
 {
     class Program
     {
+        private IPAddress _localIP;
         static void Main(string[] args)
         {
-            BikeController bc = new BikeController("");
-            bc.ResetBike();
-            while (true)
-            {
-                String[] stat = bc.GetStatus();
-                Console.Write("Heartrate: " + stat[(int)Enums.StatusInfo.HEARTRATE]);
-                Console.Write(" Power: "+ stat[(int)Enums.StatusInfo.POWER]);
-                Console.Write(" Distance: " + stat[(int)Enums.StatusInfo.DISTANCE]);
-                Console.WriteLine(" RPM: " + stat[(int)Enums.StatusInfo.RPM]);
-            }
-            /*
-            ComController cc = new ComController("COM5");
-            cc.openConnection();
-            cc.send("rs");
-            Console.WriteLine((cc.read()));
-            cc.send("cm");
-            Console.WriteLine((cc.read()));
-            cc.send("pw 400");
-            while (true)
-            {
-                cc.send("st");
-                Console.WriteLine((cc.read()));
-               // Console.ReadKey();
-            }
-             * */
+            new Program();
         }
-    }
+
+        public Program()
+        {
+            _localIP = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(_localIP, 11000);
+
+			server.Start();
+			while (true)
+			{
+				TcpClient tcpClient = server.AcceptTcpClient();
+
+				new Thread(() =>
+				{
+					BinaryFormatter formatter = new BinaryFormatter();
+					while (true)
+					{
+						//Packet packet = (Packet)formatter.Deserialize(tcpClient.GetStream());
+						//packet.handleServer(tcpClient, this);
+					}
+				}).Start();
+			}
+		}
+
+
+		void sendPacket(TcpClient client, String p)
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize(client.GetStream(), p);
+		}
+       }
+    
 }
