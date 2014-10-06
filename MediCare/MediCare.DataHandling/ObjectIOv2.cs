d52c8e5c56d27da0a3f9718a2f0b0d1c1c94fc71
justin <oxygen.crew@gmail.com>
2014-10-06 14:02:11 +0200

    started encrypted data saving

---
+++ MediCare/MediCare.DataHandling/ObjectIOv2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediCare.DataHandling
+{
+    public class ObjectIOv2
+    {
+        private ArrayList measurements { get; set; };
+        private string timestamp = DateTime.Now.ToString();
+
+        public ObjectIOv2()
+        {
+            measurements = new ArrayList();
+        }
+
+        public void add(Measurement m)
+        {
+            measurements.Add(m);
+        }
+    }
+}
