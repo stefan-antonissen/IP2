285f60b34b26a170796fd7636f92f0e32ec82b6a
rudyjason <rudyjason.ep@hotmail.com>
2014-10-21 12:14:42 +0200

    comments for IPs

--- MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
+++ MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
@@ -8,6 +8,9 @@ namespace MediCare.NetworkLibrary
 {
     public static class NetworkSettings
     {
+        //Local: 127.0.0.1
+        //Jack: 145.102.76.200
+        //Standard port: 11000
         public const string SERVERIP = "127.0.0.1";
         public const int SERVERPORT = 11000;
 
