baeb4c5afb85e33f9a47b0e54ad97fbcc0eeeab4
rudyjason <rudyjason.ep@hotmail.com>
2014-10-21 12:20:36 +0200

    rudy is nu server, biatches

--- MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
+++ MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
@@ -11,8 +11,7 @@ namespace MediCare.NetworkLibrary
         //Local: 127.0.0.1
         //Jack: 145.102.76.200
         //Standard port: 11000
-        public const string SERVERIP = "145.102.76.200";
-        public const int SERVERPORT = 11000;
-
+        public const string SERVERIP = "145.102.68.138";
+        public const int SERVERPORT = 11000; 
     }
 }
