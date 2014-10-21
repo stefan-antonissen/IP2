3e63296632f0e60b1aecf2c969ae620a34aab7e4
rudyjason <rudyjason.ep@hotmail.com>
2014-10-21 12:15:11 +0200

    oops

--- MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
+++ MediCare/MediCare.NetworkLibrary/NetworkSettings.cs
@@ -11,7 +11,7 @@ namespace MediCare.NetworkLibrary
         //Local: 127.0.0.1
         //Jack: 145.102.76.200
         //Standard port: 11000
-        public const string SERVERIP = "127.0.0.1";
+        public const string SERVERIP = "145.102.76.200";
         public const int SERVERPORT = 11000;
 
     }
